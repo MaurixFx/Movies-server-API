7e59ad625a34d79ad9d5460499f39171f2fa8574
Mauricio Figueroa <maurixfx@gmail.com>
2023-11-26 23:38:43 +0100

    GET reviews route

--- Sources/App/Controllers/ReviewController.swift
+++ Sources/App/Controllers/ReviewController.swift
@@ -11,7 +11,12 @@ import Vapor
 struct ReviewController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let review = routes.grouped("reviews")
+        review.get(use: index)
         review.post(use: create)
+    }
+    
+    func index(req: Request) async throws -> [Review] {
+        try await Review.query(on: req.db).all()
     }
 
     func create(req: Request) async throws -> Review {
